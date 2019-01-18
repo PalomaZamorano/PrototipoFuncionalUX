0ad3b04524c4d3f9725d222832707e86c95f0909
Paloma ZAmorano <paloma.zamorano@usach.cl>
2019-01-18 02:16:17 -0300

    Primer commit con vista principal en vuejs

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+
+  <div id="page-wrapper">
+
+  <div class="col-md-14 ml-auto">  
+  <md-toolbar class="md-accent" md-elevation="1" style="background-color: #3BB9D8;">
+
+    <img class = "md-title" src="images/icono.png" alt="Icono"
+								 style="width:50px;height:50px;" >
+      
+      
+         
+      <input  type="text" placeholder="Buscar..."> 
+      <md-button class="md-icon-button">
+        <v-icon medium>add_circle</v-icon>
+      </md-button>          
+      <md-button class="md-icon-button">
+        <v-icon medium>notifications</v-icon>
+      </md-button>
+      <md-button class="md-icon-button">
+        <v-icon medium>account_box</v-icon>
+      </md-button>
+      
+    </md-toolbar>
+</div>
+
+
+<div id="navflotante" class="col-md-14 ml-auto">
+  <md-toolbar class="md-accent" md-elevation="1" style="background-color: #3BB9D8;">
+      <md-button to="" >Perdidos</md-button>
+      <md-button  to="" >Mapa</md-button>
+      <md-button  to="" >Encontrados</md-button>
+    </md-toolbar>
+</div>
+    
+    
+    <br/>
+    <br/>
+   
+<main class="container">
+      <router-view></router-view>
+    </main>
+
+
+
+
+
+    
+</div>
+
+ 
+
+
+</template>
+
+<script>
+
+import Vue from 'vue';
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+
+
+
+export default {
+
+}
+</script>
+
+<style>
+	
+			#navflotante{
+        z-index:10;
+				overflow: hidden;
+        background-color: #333;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+				}
+      .active{
+       float: left;
+      display: block;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 17px;
+      }
+      .md-toolbar + .md-toolbar {
+         margin-top: 16px;
+      }
+   
+			#search {
+				  display: block;
+				  color: white;
+				  text-align: center;
+				  padding:8px 12px;
+				  text-decoration: none;
+				}	
+
+
+			
+				
+				</style>
+        
---
+++ src/Index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="row">
+
+    <div class="col-6 col-12-narrower">
+    <md-card>
+      <md-card-header>
+        <md-avatar>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2QRbAm-HvLlVuDqXq-Xf9d1yywpv_wzXA2Ghga-38VqqzXh3pmQ" alt="Avatar">
+        </md-avatar>
+
+        <div class="md-title">Title goes here</div>
+        <br/>
+      </md-card-header>
+
+      
+      <md-card-media>
+        <img src="https://s3-eu-west-1.amazonaws.com/barkibu-blog/blog+images/mi-perro-tiene-hipo-muy-seguido-que-le-pasa/perros-hipo-2.jpg" class="responsive" alt="Norway">
+      </md-card-media>
+        <br/>
+      <div class="well well-sm"> 
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea nostrum.
+       
+      </md-card-content>
+
+      <md-card-actions>
+         
+          <md-button class="md-icon-button">
+             <v-icon medium>comment</v-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+             <v-icon medium>share</v-icon>
+          </md-button>
+        </md-card-actions>
+     </div>   
+    </md-card>
+
+    </div>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .md-card {
+    overflow: hidden;
+    width: 320px;
+    margin: 10px;
+    vertical-align: top;
+    overflow: hidden;
+    padding: 16px;
+
+  }
+  .responsive {
+            height: auto;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+     
+  
+
+</style>
+
+<script>
+import Vue from 'vue';
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+Vue.use(VueMaterial)
+
+
+export default {
+  name: 'Media'
+}
+</script>
+
